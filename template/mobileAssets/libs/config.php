869713b6d7cdaadfe9066f207ad482caf435a1da
sabetech <kofexdynamxstudios@gmail.com>
2013-06-18 20:22:50 +0000

    Create config.php

---
+++ template/mobileAssets/libs/config.php
@@ -0,0 +1,10 @@
+<?
+$defaultFormat='jpg';
+$defaultWidth=120;
+$defaultHeight=120;
+/*
+DeviceAtlas.json is the major device repository provided by DeviceAtlas. 
+$defaultJSON holds the relative path of this file.
+*/
+$defaultJSON='sample\json\DeviceAtlas.json';
+?>
